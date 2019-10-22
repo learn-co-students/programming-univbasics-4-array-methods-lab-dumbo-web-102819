641e553f6ba84a68798d728b8a2fe4d6db520b1e
Eric Cave <caveman4949@gmail.com>
2019-10-22 23:09:24 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,13 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
-def using_sort(array)
-
+def using_sort = (array, element, string)
+using_sort.sort
 end
 
-def using_reverse(array)
-
+def using_reverse = (array, element, string)
+using_reverse.reverse
 end
 
 def using_first(array)
